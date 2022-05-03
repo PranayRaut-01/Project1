0e2ccbeed0ec39b9e8ead39d0d3377567a2314cf
PranayRaut-01 <102968236+PranayRaut-01@users.noreply.github.com>
2022-05-04 01:03:39 +0530

    few fixes

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -102,8 +102,8 @@ const updateBlog = async function (req, res) {
     if (blog) {
       if (blog.isDeleted === false) {
         if (blog.isPublished === false) {
-          const updatedDate = await blogModel.findOneAndUpdate({ _id: blogId }, { $set: { isPublished: true, publishedAt: Date.now() } },{new:true});
-          return res.status(200).send({ status: true, data: updatedDate });
+          const updatedDate = await blogModel.findOneAndUpdate({ _id: blogId }, { $set: { isPublished: true, publishedAt: Date.now() } });
+ 
         }
 
         const updatedBlog = await blogModel.findOneAndUpdate({ _id: blogId }, { ...data }, { new: true });
